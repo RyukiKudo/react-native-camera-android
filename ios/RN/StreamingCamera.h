41587e6d8050a5f6a44200440fbb2aff206881bb
ryuki kudo <>
2018-09-26 18:40:05 +0900

    一時保存

---
+++ ios/RN/StreamingCamera.h
@@ -0,0 +1,53 @@
+#import <AVFoundation/AVFoundation.h>
+#import <React/RCTBridge.h>
+#import <React/RCTBridgeModule.h>
+#import <UIKit/UIKit.h>
+
+@class StreamingCamera;
+
+@interface StreamingCamera : UIView <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property(nonatomic, strong) dispatch_queue_t sessionQueue;
+@property(nonatomic, strong) AVCaptureSession *session;
+@property(nonatomic, strong) AVCaptureDeviceInput *videoCaptureDeviceInput;
+@property(nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
+@property(nonatomic, strong) AVCaptureMovieFileOutput *movieFileOutput;
+@property(nonatomic, strong) AVCaptureMetadataOutput *metadataOutput;
+@property(nonatomic, strong) id runtimeErrorHandlingObserver;
+@property(nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+@property(nonatomic, strong) NSArray *barCodeTypes;
+
+@property(nonatomic, assign) NSInteger presetCamera;
+@property (assign, nonatomic) NSInteger flashMode;
+@property (assign, nonatomic) CGFloat zoom;
+@property (assign, nonatomic) NSInteger autoFocus;
+@property (assign, nonatomic) float focusDepth;
+@property (assign, nonatomic) NSInteger whiteBalance;
+@property (assign, nonatomic) AVCaptureSessionPreset pictureSize;
+@property (nonatomic, assign) BOOL isReadingBarCodes;
+@property (nonatomic, assign) BOOL isDetectingFaces;
+@property(assign, nonatomic) AVVideoCodecType videoCodecType;
+@property (assign, nonatomic) AVCaptureVideoStabilizationMode videoStabilizationMode;
+
+@property (strong, nonatomic) AVCaptureVideoDataOutput *videoDataOutput;
+
+- (id)initWithBridge:(RCTBridge *)bridge;
+- (void)updateType;
+- (void)updateFlashMode;
+- (void)updateFocusMode;
+- (void)updateFocusDepth;
+- (void)updateZoom;
+- (void)updateWhiteBalance;
+- (void)updatePictureSize;
+- (void)takePicture:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)record:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
+- (void)stopRecording;
+- (void)resumePreview;
+- (void)pausePreview;
+- (void)onReady:(NSDictionary *)event;
+- (void)onMountingError:(NSDictionary *)event;
+- (void)onPictureSaved:(NSDictionary *)event;
+- (void)onStreaming:(NSDictionary *)event;
+
+@end
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Camera from './Camera';
+import RNCamera from './RNCamera';
+import StreamingCamera from './StreamingCamera';
+import FaceDetector from './FaceDetector';
+
+export { RNCamera, FaceDetector, StreamingCamera };
+
+export default Camera;
